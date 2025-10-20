e2bfb87bb8a68d2e47ff2b5e8139bd25c14560a2
KushalPathave02 <kushalpathave54@gmail.com>
2025-10-21 01:45:57 +0530

    Fix CORS: Add back correct frontend URL to allowed origins

--- flask_server/app.py
+++ flask_server/app.py
@@ -31,6 +31,7 @@ allowed_origins = [
   'http://localhost:3001',
   'https://loopr-1.onrender.com',
   'https://major-project-btech-1.onrender.com',
+  'https://major-project-btech.onrender.com'
 ]
 
 # If a public base URL is set (e.g., http://192.168.1.60:5000),
